3fcea0d774625ac251843b9c1141d64df0f22119
ethan-x11 <subhamdutta96740@gmail.com>
2023-07-31 17:04:17 +0530

    .

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
 
 cap = cv2.VideoCapture(0)
 
-
-
 while True:
     _, frame = cap.read()
     labels = []
@@ -24,8 +22,6 @@ while True:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
         roi_gray = gray[y:y+h,x:x+w]
         roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
-
-
 
         if np.sum([roi_gray])!=0:
             roi = roi_gray.astype('float')/255.0
